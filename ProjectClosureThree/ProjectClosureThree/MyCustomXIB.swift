9eb0fa44c0d8176025b37f017fcf6416e0fc3fe2
Aninha <aninhapss2000@gmail.com>
2023-03-23 21:53:19 -0300

    Added MyCustomXIB e DataSource

---
+++ ProjectClosureThree/ProjectClosureThree/MyCustomXIB.swift
@@ -0,0 +1,23 @@
+//
+//  MyCustomXIB.swift
+//  ProjectClosureThree
+//
+//  Created by Ana Paula Silva de Sousa on 23/03/23.
+//
+
+import UIKit
+
+class MyCustomXIB: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
